412493764a6afb1500aee19a2f7f3d5aff57e0eb
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-14 23:15:12 +0530

    Create Variable.java

---
+++ 01_Variable and Data Types/Variable.java
@@ -0,0 +1,14 @@
+public class Variable{
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 5;
+
+        int c = 2 * (a + b);
+
+        // 2 is literal, if any constant directly use in any opertaion is called literal
+        // a and b is Variable
+
+        System.out.print("Result is: "+ c);
+    }
+}
