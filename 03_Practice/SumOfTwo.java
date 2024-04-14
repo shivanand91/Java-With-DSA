b5461f68b421275735610ce628c34cbb83683560
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-14 23:15:33 +0530

    Create SumOfTwo.java

---
+++ 03_Practice/SumOfTwo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class SumOfTwo {
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter NUmer one: ");
+    int numOne = sc.nextInt();
+    System.out.print("Enter NUmer one: ");
+    int numTwo = sc.nextInt();
+
+    int sum = numOne + numTwo;
+
+    System.out.print("Sum of two number is: " + sum);
+ }   
+}
