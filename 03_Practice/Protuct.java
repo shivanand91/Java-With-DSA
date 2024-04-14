53de5a03dda6b4e4fe49996c937e7d803e1bbb32
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-14 23:15:29 +0530

    Create Protuct.java

---
+++ 03_Practice/Protuct.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Protuct {
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter NUmer one: ");
+    int numOne = sc.nextInt();
+    System.out.print("Enter NUmer one: ");
+    int numTwo = sc.nextInt();
+
+    int product = numOne * numTwo;
+
+    System.out.print("Product of two number is: " + product);
+ }   
+}
