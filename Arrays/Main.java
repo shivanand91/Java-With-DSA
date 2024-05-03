264fd2cf8ada22629d132a521b8452e38a171f66
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-05-03 14:26:39 +0530

    os problem

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -1,20 +1,28 @@
-
+import java.util.HashMap;
 
 public class Main {
+
     public static void main(String[] args) {
-        int n [] = {1, 2, 5, 5, 5, 5, 7, 8, 9};
-        int count = 0;
-        for(int i = 0; i < n.length; i++){
-     
-            if(n[i] == n[i + 1]){
-                count++;
-            }
-            else{
-                
-            }
-            
-            System.out.println(n[i]);
+      int[] n = {1, 2, 5, 5, 5, 5, 7, 8, 9};
+      int duplicateCount = countDuplicates(n);
+      System.out.println("Number of duplicate elements: " + duplicateCount);
+    }
+  
+    public static int countDuplicates(int[] arr) {
+      // Use a hash table to store element counts
+      HashMap<Integer, Integer> elementMap = new HashMap<>();
+      int count = 0;
+      
+      for (int num : arr) {
+        // Check if element exists in the map
+        if (elementMap.containsKey(num)) {
+          count++;
+        } else {
+          elementMap.put(num, 1);
         }
-        System.out.println(count);
-    } 
-}
+      }
+      
+      return count;
+    }
+  }
+  
