22312db80b909474cb2c1f3ea515cc2b4ae307db
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-05-03 14:38:55 +0530

    Function start

---
+++ 07_Function/Solution_01.java
@@ -0,0 +1,8 @@
+public class Solution_01{
+    public static void printHello(){
+        System.out.print("Hello World");
+    }
+    public static void main(String args []){
+        printHello();
+    }
+}
